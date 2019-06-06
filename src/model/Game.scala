53afe21fe2073fcd1a1310ff0bb039af323a9381
focacci <michaelfocacci@gmail.com>
2019-06-06 11:21:41 -0400

    Updated Game class

--- src/model/Game.scala
+++ src/model/Game.scala
@@ -4,21 +4,20 @@ import model.gameobjects.player.Player
 import model.gameobjects.{PhysicalObject, Platform}
 import model.physics.{Physics, PhysicsVector}
 
+
 class Game(val host: String) {
 
 
   var players: Map[String, Player] = Map()
-  addPlayer(host)
 
   var objects: List[PhysicalObject] = List()
+
   var platforms: List[Platform] = List()
 
-  val dimensions: Map[String, Int] = Map(
-    "width" -> 900,
-    "height" -> 600
-  )
+  val width: Int = 900
+  val height: Int = 600
 
-  var startingLocation: PhysicsVector = new PhysicsVector(dimensions("width").toDouble / 2, dimensions("height").toDouble)
+  var startingLocation: PhysicsVector = new PhysicsVector(width.toDouble / 2, height.toDouble)
 
   var lastUpdateTime: Long = System.nanoTime()
 
@@ -39,6 +38,19 @@ class Game(val host: String) {
     Physics.update(this)
   }
 
+  def load(): Unit = {
+    this.platforms = List(
+      new Platform(new PhysicsVector(0, height), new PhysicsVector(width, height)),
+      new Platform(new PhysicsVector(0, 400), new PhysicsVector(300, 400)),
+      new Platform(new PhysicsVector(0, 200), new PhysicsVector(300, 200)),
+      new Platform(new PhysicsVector(300, 300), new PhysicsVector(600, 300)),
+      new Platform(new PhysicsVector(300, 500), new PhysicsVector(600, 500)),
+      new Platform(new PhysicsVector(600, 400), new PhysicsVector(width, 400)),
+      new Platform(new PhysicsVector(600, 200), new PhysicsVector(width, 200))
+    )
+    
+    this.addPlayer(host)
+  }
 
 
 
